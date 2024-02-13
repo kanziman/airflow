ac00dafa60426a66b8a690ce9163f3df2b6f7200
kanziman <kanzirunner@gmail.com>
2024-02-13 14:51:32 +0900

    bash op

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -1,33 +1,22 @@
+from airflow import DAG
 import datetime
-
 import pendulum
-
-from airflow.models.dag import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.empty import EmptyOperator
 
-# Tip
-# 1. dag_id 와 file 명을 일치
-# 2. class name 과 task id 일치(bash_t1)
 with DAG(
-    dag_id="example_bash_operator",
+    dag_id="dags_bash_operator",
     schedule="0 0 * * *",
-    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
-    catchup=False,
-    # dagrun_timeout=datetime.timedelta(minutes=60),
-    tags=["example", "example2"],
-    params={"example_key": "example_value"},
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
 ) as dag:
-    
-    # [START howto_operator_bash]
     bash_t1 = BashOperator(
         task_id="bash_t1",
         bash_command="echo whoami",
     )
-    
+
     bash_t2 = BashOperator(
         task_id="bash_t2",
-        bash_command="echo $JAVA_HOME",
+        bash_command="echo $HOSTNAME",
     )
-    
+
     bash_t1 >> bash_t2
