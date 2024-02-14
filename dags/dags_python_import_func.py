78a023c4da8ae3d0f9609b22fe213ead078e6d14
kanziman <kanzirunner@gmail.com>
2024-02-14 14:10:01 +0900

    db test

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -3,9 +3,9 @@ import pendulum
 import datetime
 from airflow.operators.python import PythonOperator
 from common.common_func import get_sftp
-from common.kor_market_price import price_main
-from common.kor_market_price_support import price_support_main
-from common.kor_market_credit import credit_main
+# from common.kor_market_price import price_main
+# from common.kor_market_price_support import price_support_main
+# from common.kor_market_credit import credit_main
 with DAG(
     dag_id="dags_python_import_func",
     schedule="30 6 * * *",
@@ -13,29 +13,29 @@ with DAG(
     catchup=False
 ) as dag:
 
-    # task_get_sftp = PythonOperator(
-    #     task_id='task_get_sftp',
-    #     python_callable=get_sftp
-    # )
-    
-    # task_get_sftp2 = PythonOperator(
-    #     task_id='task_get_sftp2',
-    #     python_callable=get_sftp
-    # )
-    
-    market_price = PythonOperator(
-        task_id='market_price',
-        python_callable=price_main
+    task_get_sftp = PythonOperator(
+        task_id='task_get_sftp',
+        python_callable=get_sftp
     )
+    
+    task_get_sftp2 = PythonOperator(
+        task_id='task_get_sftp2',
+        python_callable=get_sftp
+    )
+    
+    # market_price = PythonOperator(
+    #     task_id='market_price',
+    #     python_callable=price_main
+    # )
         
-    market_price_support = PythonOperator(
-        task_id='market_price_support',
-        python_callable=price_support_main
-    )
+    # market_price_support = PythonOperator(
+    #     task_id='market_price_support',
+    #     python_callable=price_support_main
+    # )
     
-    market_credit = PythonOperator(
-        task_id='market_credit',
-        python_callable=credit_main
-    )
+    # market_credit = PythonOperator(
+    #     task_id='market_credit',
+    #     python_callable=credit_main
+    # )
     
-    market_price >> [market_price_support, market_credit]
+    # market_price >> [market_price_support, market_credit]
