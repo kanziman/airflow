87a080b3dd0df6a70588e81a4bcb81ded2715bde
kanziman <kanzirunner@gmail.com>
2024-02-14 21:49:43 +0900

    sensor added

--- dags/dags_external_task_sensor2.py
+++ dags/dags_external_task_sensor2.py
@@ -10,22 +10,50 @@ with DAG(
     schedule='0 23 * * *',
     catchup=False
 ) as dag:
+    # external_task_sensor_a = ExternalTaskSensor(
+    #     task_id='external_task_sensor_a',
+    #     external_dag_id='dags_python_import_func',
+    #     external_task_id='market_value',
+    #     allowed_states=[State.SKIPPED],
+    #     execution_delta=timedelta(hours=1),
+    #     poke_interval=10        #10초
+    # )
+
+    # market_scrap_sensor = ExternalTaskSensor(
+    #     task_id='market_scrap_sensor',
+    #     external_dag_id='dags_python_import_func',
+    #     external_task_id='market_value',
+    #     allowed_states=[State.SUCCESS],
+    #     execution_delta=timedelta(hours=1),
+    #     poke_interval=10        #10초
+    # )
+
     external_task_sensor_a = ExternalTaskSensor(
         task_id='external_task_sensor_a',
-        external_dag_id='dags_python_import_func',
-        external_task_id='market_value',
+        external_dag_id='dags_branch_python_operator',
+        external_task_id='task_a',
         allowed_states=[State.SKIPPED],
         execution_delta=timedelta(hours=1),
         poke_interval=10        #10초
     )
 
-    market_scrap_sensor = ExternalTaskSensor(
-        task_id='market_scrap_sensor',
-        external_dag_id='dags_python_import_func',
-        external_task_id='market_value',
-        allowed_states=[State.SUCCESS],
+    external_task_sensor_b = ExternalTaskSensor(
+        task_id='external_task_sensor_b',
+        external_dag_id='dags_branch_python_operator',
+        external_task_id='task_b',
+        failed_states=[State.SKIPPED],
         execution_delta=timedelta(hours=1),
         poke_interval=10        #10초
     )
 
-    external_task_sensor_a >> market_scrap_sensor
+    external_task_sensor_c = ExternalTaskSensor(
+        task_id='external_task_sensor_c',
+        external_dag_id='dags_branch_python_operator',
+        external_task_id='task_c',
+        allowed_states=[State.SUCCESS],
+        execution_delta=timedelta(hours=1),
+        poke_interval=10        #10초
+    )
+    
+    
+    external_task_sensor_a >> external_task_sensor_b
