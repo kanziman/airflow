58046e6a5c43463ca1afa64baaa1e77d5116d715
kanziman <kanzirunner@gmail.com>
2024-02-14 21:39:01 +0900

    sensor added

--- dags/dags_external_task_sensor2.py
+++ dags/dags_external_task_sensor2.py
@@ -13,6 +13,7 @@ with DAG(
     external_task_sensor_a = ExternalTaskSensor(
         task_id='external_task_sensor_a',
         external_dag_id='dags_python_import_func',
+        external_task_id='market_value',
         allowed_states=[State.SKIPPED],
         execution_delta=timedelta(hours=1),
         poke_interval=10        #10초
@@ -21,6 +22,7 @@ with DAG(
     market_scrap_sensor = ExternalTaskSensor(
         task_id='market_scrap_sensor',
         external_dag_id='dags_python_import_func',
+        external_task_id='market_value',
         allowed_states=[State.SUCCESS],
         execution_delta=timedelta(hours=1),
         poke_interval=10        #10초
