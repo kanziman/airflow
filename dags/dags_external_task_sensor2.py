71a45d76c01ce6188d314ff83c8e2d498e2702cf
kanziman <kanzirunner@gmail.com>
2024-02-14 22:16:54 +0900

    sensor added

--- dags/dags_external_task_sensor2.py
+++ dags/dags_external_task_sensor2.py
@@ -7,14 +7,14 @@ from airflow.utils.state import State
 with DAG(
     dag_id='dags_external_task_sensor2',
     start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
-    schedule='0 23 * * *',
+    schedule='0 2 * * *',
     catchup=False
 ) as dag:
 
 
     external_task_sensor_a = ExternalTaskSensor(
         task_id='external_task_sensor_a',
-        external_dag_id='dags_branch_python_operator2',
+        external_dag_id='dags_branch_python_operator',
         external_task_id='task_a',
         allowed_states=[State.SKIPPED],
         execution_delta=timedelta(hours=1),
@@ -23,7 +23,7 @@ with DAG(
 
     external_task_sensor_b = ExternalTaskSensor(
         task_id='external_task_sensor_b',
-        external_dag_id='dags_branch_python_operator2',
+        external_dag_id='dags_branch_python_operator',
         external_task_id='task_b',
         failed_states=[State.SKIPPED],
         execution_delta=timedelta(hours=1),
@@ -32,7 +32,7 @@ with DAG(
 
     external_task_sensor_c = ExternalTaskSensor(
         task_id='external_task_sensor_c',
-        external_dag_id='dags_branch_python_operator2',
+        external_dag_id='dags_branch_python_operator',
         external_task_id='task_c',
         allowed_states=[State.SUCCESS],
         execution_delta=timedelta(hours=1),
